be65b8adad7c5f918826d300013a70d5c4964a94
Antony_Ndungu <antony.ndungu.maina@gmail.com>
2017-11-18 19:23:53 +0300

    Start Front-End development

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ import express from "express";
 import api from "./api";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import path from "path";
+import fs from "fs";
+import sassMiddleware from "node-sass-middleware";
 import morgan from "morgan";
 
 mongoose.connect(config.database, err => {
@@ -18,6 +21,17 @@ const server = express();
 server.use(bodyParser.json());
 server.use(morgan('dev'));
 
+server.use(sassMiddleware({
+    src: path.join(__dirname, "sass"),
+    dest: path.join(__dirname, "public"),
+    outputStyle: "compressed"
+}));
+
 server.use("/api", api);
+server.use(express.static("public"));
+
+/*server.get("/dashboard", (req, res) => {
+    fs.createReadStream(path.join(__dirname, "public/dashboard.html")).pipe(res);
+});*/
 
 server.listen(config.port, () => console.log("Server listening on port", config.port));
